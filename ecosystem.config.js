9af1ca9cb2c4a89381ed673005b5e4d9254dc217
cread <charlesread@clayton.edu>
2017-11-29 14:08:03 -0500

    deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,5 +5,14 @@ module.exports = {
       script    : 'index.js',
       interpreter: '/home/node/.nvm/versions/node/v8.9.1/bin/node'
     }
-  ]
+  ],
+  deploy: {
+    production: {
+      user: 'node',
+      host: '174.138.79.67',
+      ref: 'origin/master',
+      repo: 'git@github.com:charlesread/watermelon.git',
+      path: '/home/node/apps/watermelon'
+    }
+  }
 };
