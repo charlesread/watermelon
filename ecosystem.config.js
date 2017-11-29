1336506ca41524d61544016769ad3e180dd5110d
charlesread <charles@charlesread.com>
2017-11-29 13:53:39 -0500

    pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [
+    {
+      name      : 'watermelon-8000',
+      script    : 'index.js',
+      interpreter: '/home/node/.nvm/versions/node/v8.9.1/bin/node'
+    }
+  ]
+};
