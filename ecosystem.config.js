51d668e8cc2c288a5baa465d070c338a828e3954
cread <charlesread@clayton.edu>
2017-11-29 14:20:50 -0500

    axed deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,14 +5,5 @@ module.exports = {
       script    : 'index.js',
       interpreter: '/home/node/.nvm/versions/node/v8.9.1/bin/node'
     }
-  ],
-  deploy: {
-    production: {
-      user: 'cread',
-      host: '174.138.79.67',
-      ref: 'origin/master',
-      repo: 'git@github.com:charlesread/watermelon.git',
-      path: '/home/node/apps/watermelon'
-    }
-  }
+  ]
 };
