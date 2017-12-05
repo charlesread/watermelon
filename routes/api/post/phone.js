d395e665132bbc5675db66a7d5ce2960d7c4268a
Charles Read <physixrox@gmail.com>
2017-12-04 21:06:24 -0500

    twilio

--- routes/api/post/phone.js
+++ routes/api/post/phone.js
@@ -5,6 +5,7 @@ const boom = require('boom')
 const db = require('~/lib/db')
 const sql = require('~/lib/sql')
 const userUtil = require('~/util/user')
+const sms = require('~/lib/sms')
 
 module.exports = [
   {
@@ -18,6 +19,10 @@ module.exports = [
       db.query(sql.phone.insert, [req.payload.phone, user.id])
         .then(function () {
           reply()
+          sms.send(req.payload.phone, `Hey, ${user.first_name}! Thanks for updating you\'re mobile number - we\'ll be in touch!`)
+            .catch(function (err) {
+              console.error('error sending %s a text: %s', req.payload.phone, err.message)
+            })
         })
         .catch(function (err) {
           console.error(err.message)
