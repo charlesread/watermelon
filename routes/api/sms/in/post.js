1ed74318d20cb3f255a5b942287a67f3bbe3d912
Charles Read <physixrox@gmail.com>
2017-12-22 08:40:12 -0500

    working on sms in

---
+++ routes/api/sms/in/post.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const boom = require('boom')
+
+const db = require('~/lib/db')
+const sql = require('~/lib/sql')
+const sms = require('~/lib/sms')
+
+module.exports = [
+  {
+    method: 'post',
+    path: '/api/sms/in,
+    handler: function (req, reply) {
+      (async function () {
+        console.log(req.payload)
+        reply()
+      })()
+        .catch(function (err) {
+          log.error(err.message)
+          log.debug(err.stack)
+          reply(boom.badRequest())
+        })
+    }
+  }
+]
